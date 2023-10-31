c0c9db479074e9e676304c62c59dc5521e72bed8
Вадим Королёв <pydim@mail.ru>
2023-10-31 16:43:26 +0300

    В скрипт обновления добавлен rasp.doc

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
+# Скачать rasp и спарсить его
+wget http://www.vpmt.ru/docs/rasp.doc -O /tmp/schedule.doc
+unoconv -d document --format=docx /tmp/schedule.doc
+php /home/sysadmin/Texbot-php/schedule_parser.php
+
+# Скачать rasp2, который, в случае неточностей перезапишет неправильные данные
 wget http://www.vpmt.ru/docs/rasp2.doc -O /tmp/schedule.doc
 unoconv -d document --format=docx /tmp/schedule.doc
 php /home/sysadmin/Texbot-php/schedule_parser.php
